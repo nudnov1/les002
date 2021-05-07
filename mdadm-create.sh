2a0ffd224967ab2ebc3ddf8f69744adf2ee0c783
nudnov1 <81551670+nudnov1@users.noreply.github.com>
2021-05-07 16:06:08 +0300

    Delete mdadm-create.sh

--- mdadm-create.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# 1. Create RAID 1
-mdadm --create /dev/md/raid1_tst --level=1 --raid-device=2 /dev/sdb /dev/sdc
-# 2. #Add disk to raid:
-mdadm --add /dev/md/raid1_tst /dev/sdd
